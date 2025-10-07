f92cfa560da38b95147994d71ea863d3dcae75d3
mink666 <10421036@student.vgu.edu.vn>
2025-10-08 00:57:45 +0700

    Fix

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers;
 
-// Import Laravel's core base controller
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
+
 }
