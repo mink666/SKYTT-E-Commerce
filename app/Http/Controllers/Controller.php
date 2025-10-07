c5904877a8d18cf8a46ed7d66dece10672906993
mink666 <10421036@student.vgu.edu.vn>
2025-10-08 00:52:13 +0700

    Fix layout

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+// Import Laravel's core base controller
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
 
 Route::get('/', [PageController::class, 'home'])->name('home');
 Route::get('/about', [PageController::class, 'about'])->name('about');
